00ea0a378b46469191612466ee9be63af49f689b
Dan <1850484@cegep-heritage.qc.ca>
2023-03-22 10:11:47 -0400

    change hbs and title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hello! My name is Dan!' });
+});
+router.get('/:msg', function(req, res, next) {
+  res.render('index', { title: req.params.msg });
 });
 
 module.exports = router;
